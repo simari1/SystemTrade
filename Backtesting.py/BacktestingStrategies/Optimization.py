f1f0d714afaa58c403ccbef65ce39058db8eb836
simari <bdfsg610@yahoo.co.jp>
2022-09-20 23:25:08 +0900

    add optimization

--- Backtesting.py/BacktestingStrategies/Optimization.py
+++ Backtesting.py/BacktestingStrategies/Optimization.py
@@ -1,5 +1,16 @@
 def optim_func(series):
-    if series["# Trades"] < 5:
+    if series["# Trades"] < 15:
         return -1
-    
-    return series["Equity Final [$]"] / series["Exposure Time [%]"]
+    if series["Max. Drawdown [%]"] > 10:
+        return -1
+    if series["Win Rate [%]"] < 60:
+        return -1
+    if series["Worst Trade [%]"] < -20:
+        return -1
+    if series["Profit Factor"] < 1:
+        return -1
+    if series["Sharpe Ratio"] < 1.5:
+        return -1
+    if series["SQN"] < 2:
+        return -1
+    return series["Equity Final [$]"]
