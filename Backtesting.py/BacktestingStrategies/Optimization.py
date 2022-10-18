3b575d720a2309ab34b1e47bfb5fee880f0d5b4f
simari <bdfsg610@yahoo.co.jp>
2022-10-18 17:09:29 +0900

    Prod strategy

--- Backtesting.py/BacktestingStrategies/Optimization.py
+++ Backtesting.py/BacktestingStrategies/Optimization.py
@@ -1,16 +1,18 @@
 def optim_func(series):
-    if series["# Trades"] < 7:
-        return -1
-    if series["Max. Drawdown [%]"] > 20:
-        return -1
-    if series["Win Rate [%]"] < 60:
-        return -1
-    if series["Worst Trade [%]"] < -20:
-        return -1
+    # if series["# Trades"] < 7:
+    #     return -1
+    # if series["Max. Drawdown [%]"] > 20:
+    #     return -1
+    # if series["Win Rate [%]"] < 40:
+    #     return -1
+    # if series["Worst Trade [%]"] < -20:
+    #     return -1
     if series["Profit Factor"] < 1:
         return -1
-    if series["Sharpe Ratio"] < 1.5:
+    # if series["Sharpe Ratio"] < 1.5:
+    #     return -1
+    if series["SQN"] < 2.5:
         return -1
-    if series["SQN"] < 2:
+    if series["Expectancy [%]"] < 0:
         return -1
     return series["Equity Final [$]"]
---
+++ Backtesting.py/BacktestingStrategies/Prod_Strategy_Rsi.py
@@ -0,0 +1,37 @@
+from backtesting import Strategy
+from backtesting.lib import crossover
+import talib as ta
+from backtesting.lib import crossover
+
+class RsiOscillator(Strategy):
+    upper_bound=65
+    lower_bound=40
+    rsi_window=12
+    atr_window=14
+    atr_stoploss=4
+    atr_takeprofit=8
+
+    def init(self):
+        # RSI = ta.RSI(close, timeperiod=14)
+        # param 1 --- function to calculate indicator values
+        # param 2 --- pass data
+        self.rsi = self.I(ta.RSI, self.data.Close, self.rsi_window)
+        self.atr = self.I(ta.ATR, self.data.High , self.data.Low, self.data.Close, self.atr_window)
+
+    def next(self): # チャートデータの行ごとに呼び出される
+        if self.position:
+            price = self.trades[-1].entry_price
+            if self.data.Close[-1] > price + (self.atr[-1] * self.atr_takeprofit) or\
+                self.data.Close[-1] < price - (self.atr[-1] * self.atr_stoploss):
+                if self.data.Close[-1] > price + (self.atr[-1] * self.atr_takeprofit):
+                    print("take profit  " + str(self.data.index[-1]) + "  " + str(price) + "  " + str(price + (self.atr[-1] * self.atr_takeprofit)))
+                if self.data.Close[-1] < price - (self.atr[-1] * self.atr_stoploss):
+                    print("stop loss  " + str(self.data.index[-1]) + "  " + str(price) + "  " + str(price - (self.atr[-1] * self.atr_stoploss)))
+                self.position.close()
+        elif crossover(self.rsi, self.upper_bound):
+            if self.position:
+                print("RSI close " + str(self.data.index[-1]))
+                self.position.close()
+        elif crossover(self.lower_bound, self.rsi):
+            if not self.position:
+                self.buy() # 買い
